659321ebe472cb106db78efa00e6a81fc53e4a9a
Mizogamii <120470907+Mizogamii@users.noreply.github.com>
2024-12-09 11:03:54 -0300

    Update Acessibilidade.java

--- src/main/java/projeto/frontend/utils/Acessibilidade.java
+++ src/main/java/projeto/frontend/utils/Acessibilidade.java
@@ -10,14 +10,13 @@ public class Acessibilidade {
      * @param label O label no qual o foco será configurado.
      */
     public static void configurarEstiloFoco(Label label) {
-        String estiloOriginal = label.getStyle(); // Pega o estilo original da label
+        String estiloOriginal = label.getStyle();
 
         label.focusedProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue) {
-                // Mantém o estilo original e adiciona o estilo de foco
+
                 label.setStyle(estiloOriginal + "-fx-border-color: blue; -fx-border-width: 2px; -fx-border-radius: 5px;");
             } else {
-                // Volta ao estilo original sem o foco
                 label.setStyle(estiloOriginal);
             }
         });
